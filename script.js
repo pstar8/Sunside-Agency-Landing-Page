69faf05e402a09238cdecc2b23afb87f643fa4d3
ajitechcode <143255626+pstar8@users.noreply.github.com>
2025-05-17 18:04:26 +0100

    updated styles and JS

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-// $(document).ready(function () {
-//   $('.menu-btn').click(displayNav);
-//   function displayNav() {
-//     $('.nav-links').toggleClass('show');
-//     $('.menu-btn').toggle(2000);
-//     $('body').toggleClass('lock-scroll');
-//   }
-// });
-
-// // Github Cheosere's Code JS
-// // const menuMobileButton = document.getElementById('menu_mobile')
-// // const menuMobile = document.getElementById('navbar_menu_mobile')
-
-// // menuMobileButton.addEventListener('click', () => {
-// //     menuMobileButton.classList.toggle('menu_close')
-// //     menuMobile.classList.toggle('navbar_show')
-// // })
-
-// // anime({
-// //     targets: '.header_title_arrow',
-// //     translateY: [-200, 500],
-// //     easing: 'easeInOutSine',
-// //     direction: 'alternate',
-// //     loop: true
-// // });
+$(document).ready(function () {
+  $('.menu-btn').click(function () {
+    $('.nav-links').toggleClass('show');
+    $('.menu-btn').toggleClass('menu_close');
+    $('.nav-links').toggleClass('navbar_show');
+    $('body').toggleClass('lock-scroll');
+  });
+});
